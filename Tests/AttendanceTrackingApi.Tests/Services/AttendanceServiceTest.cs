96a0495f0140fdd2b0fafa23679ffee251c67e11
Godwin Sei Essiam <164816138+seigodwin@users.noreply.github.com>
2026-06-29 16:24:43 +0000

    Initiate unit tests

---
+++ Tests/AttendanceTrackingApi.Tests/Services/AttendanceServiceTest.cs
@@ -0,0 +1,20 @@
+
+using FluentAssertions;
+using Xunit;
+
+namespace AttendanceTrackingApi.Tests.Services
+{
+    public class AttendanceServiceTest
+    {
+        [Fact]
+        public void LoginAsync_WithValidInput_ReturnsSuccess()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            true.Should().BeTrue();
+        }
+    }
+}
